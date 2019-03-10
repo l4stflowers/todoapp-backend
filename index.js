5c24ab0c4b279372515b1b9b9b4d88fba62012c0
l4stflowers <l4stflowers@gmail.com>
2019-03-10 11:35:50 +0900

    fix lint settings

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+const serverless = require('serverless-http')
+const express = require('express')
+const app = express()
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
