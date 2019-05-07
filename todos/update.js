2c8b3624bafabc76cea2ac4e72852d3ec072c9fd
l4stflowers <l4stflowers@gmail.com>
2019-05-07 21:46:59 +0900

    Add updating a specified todo future.

--- todos/update.js
+++ todos/update.js
@@ -1,8 +1,48 @@
 'use strict'
 
-module.exports.create = (event, context, callback) => {
-  const response = {
-    statusCode: 204
+const AWS = require('aws-sdk')
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.update = (event, context, callback) => {
+  const body = JSON.parse(event.body)
+
+  // TODO Add validations.
+
+  const params = {
+    TableName: process.env.TODO_TABLE,
+    ExpressionAttributeNames: {
+      '#title': 'title',
+      '#memo': 'memo',
+      '#state': 'state'
+    },
+    ExpressionAttributeValues: {
+      ':titleVal': body.title,
+      ':memoVal': body.memo,
+      ':stateVal': body.state
+    },
+    Key: {
+      userId: event.pathParameters.userId,
+      id: event.pathParameters.id
+    },
+    UpdateExpression: 'SET #title = :titleVal, #memo = :memoVal, #state = :stateVal'
   }
-  callback(null, response)
+
+  dynamoDb.update(params, (error, result) => {
+    if (error) {
+      console.error(error)
+      // TODO Add 404 error handling.
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t update the todo.'
+      })
+      return
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes)
+    }
+    callback(null, response)
+  })
 }
