7f434193fdb7a08b05d2b46727d0576755bf138b
l4stflowers <l4stflowers@gmail.com>
2019-05-07 22:21:26 +0900

    Add delete specified todo future.

--- todos/delete.js
+++ todos/delete.js
@@ -1,8 +1,32 @@
 'use strict'
 
-module.exports.create = (event, context, callback) => {
-  const response = {
-    statusCode: 204
+const AWS = require('aws-sdk')
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.TODO_TABLE,
+    Key: {
+      userId: event.pathParameters.userId,
+      id: event.pathParameters.id
+    }
   }
-  callback(null, response)
+
+  dynamoDb.delete(params, (error) => {
+    if (error) {
+      console.error(error)
+      callback(null, {
+        statusCode: 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t remove the todo.'
+      })
+      return
+    }
+
+    const response = {
+      statusCode: 204,
+      body: JSON.stringify({})
+    }
+    callback(null, response)
+  })
 }
